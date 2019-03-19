1f4d30b7deea3077e708ef05094550b077d23e1b
Thomas Statter <thomas.statter@mapscape.eu>
2019-03-19 17:31:08 +0100

    locales

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@
 #
 #
 user = YamsCore::User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
+  user.name = 'admin'
   user.password = Rails.application.secrets.admin_password
   user.password_confirmation = Rails.application.secrets.admin_password
   user.confirm
