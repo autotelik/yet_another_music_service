52e13b2807ee1cd4b3bad95c1a15709231d34165
tom statter <dev@autotelik.co.uk>
2019-04-15 09:14:18 +0200

    playlist and album views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-#
-#
 user = YamsCore::User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
   user.name = 'admin'
   user.password = Rails.application.secrets.admin_password
@@ -15,7 +13,7 @@ puts 'CREATED ADMIN USER : ' << user.email
 
 if(Rails.env.development?)
   user = YamsCore::User.find_or_create_by!(email: 'artist@example.com') do |user|
-    user.name = 'aqwan'
+    user.name = 'yams'
     user.password = 'artist_change_me'
     user.password_confirmation = 'artist_change_me'
     user.confirm
@@ -26,5 +24,7 @@ end
 DefaultCover.create!(kind: :track).tap{ |c| c.image.attach(io: File.open("#{Rails.root}/app/assets/images/covers/white_label.jpg"), filename: 'white_label.jpg') }
 DefaultCover.create!(kind: :album).tap{ |c| c.image.attach(io: File.open("#{Rails.root}/app/assets/images/covers/white_label.jpg"), filename: 'white_label.jpg') }
 DefaultCover.create!(kind: :playlist).tap{ |c| c.image.attach(io: File.open("#{Rails.root}/app/assets/images/covers/white_label.jpg"), filename: 'white_label.jpg') }
+
+DefaultCover.create!(kind: :artist).tap{ |c| c.image.attach(io: File.open("#{Rails.root}/app/assets/images/covers/default_avatar.png"), filename: 'default_avatar.png') }
 DefaultCover.create!(kind: :user).tap{ |c| c.image.attach(io: File.open("#{Rails.root}/app/assets/images/covers/default_avatar.png"), filename: 'default_avatar.png') }
 
