6e588504bc9bf5156da76c2aeaa101b43c7dd465
tom statter <dev@autotelik.co.uk>
2019-08-09 17:49:28 +0200

    deploy stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "mail.yams.fm",
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
